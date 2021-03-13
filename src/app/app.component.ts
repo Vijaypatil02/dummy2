e23d2f822166a8cc0833af6f9534fa049a7ae2c3
VIJAY PATIL <78361332+Vijaypatil02@users.noreply.github.com>
2021-03-13 21:09:46 +0530

    Commit Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-fifthteen';
+  openside=true;
+  contentMargin = 240;
+  // @ts-ignore
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    // this.initRegForm();
+  }
+  toggleopen(){
+    this.openside = !this.openside;
+    if(!this.openside){
+      this.contentMargin = 70;
+    }else{
+      this.contentMargin = 240;
+    }
+  }
+
+  // tslint:disable-next-line:typedef
+  // initRegForm() {
+  //   this.registrationForm = this.fb.group({
+  //     email: ['', [Validators.required, Validators.email]],
+  //     password: ['', Validators.required],
+  //     confirmpassword: ['', Validators.required]
+  //   });
+  // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule, MatNavList} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import { ServicesComponent } from './services/services.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
